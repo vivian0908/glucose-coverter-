9083fc406f66f8ec2eb542de51e54cb4fee9ad43
vivian0908 <95833453+vivian0908@users.noreply.github.com>
2025-09-22 11:31:10 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,31 @@
+// Updated Service Worker (cache-bust)
+const CACHE_NAME = 'bgc-v3'; // bump this version when deploying new files
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('bgc-v1').then(cache => cache.addAll([
-      './',
-      './index.html',
-      './manifest.webmanifest',
-      './icon-192.png',
-      './icon-512.png'
-    ]))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
   );
 });
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => {
+      if (k !== CACHE_NAME) return caches.delete(k);
+    }))).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
